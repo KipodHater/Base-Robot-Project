48346e7f8417127e5c88561a4858a03ecf8999ee
KipodHater <doodidoodi1900@gmail.com>
2024-12-18 19:46:23 +0200

    moved things to util

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -1,7 +1,7 @@
 package frc.robot.subsystems;
 
-import frc.robot.SwerveModule;
 import frc.lib.util.NavxGyro;
+import frc.lib.util.SwerveModule;
 import frc.robot.Constants;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
